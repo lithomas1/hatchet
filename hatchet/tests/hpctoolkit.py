097f9d3e4e5b1c4a5a62fc1a46dd6945c79bfa75
Thomas Li <47963215+lithomas1@users.noreply.github.com>
2024-04-24 10:40:48 -0400

    more black

--- hatchet/tests/hpctoolkit.py
+++ hatchet/tests/hpctoolkit.py
@@ -239,7 +239,9 @@ def test_inclusive_time_calculation(data_dir, calc_pi_hpct_db):
 @pytest.mark.parametrize("sparse_format", [False, True])
 def test_graphframe_v4(data_dir, calc_pi_hpct_v4_db, sparse_format):
     """Sanity test a GraphFrame object with known data."""
-    gf = GraphFrame.from_hpctoolkit(str(calc_pi_hpct_v4_db), sparse_format=sparse_format)
+    gf = GraphFrame.from_hpctoolkit(
+        str(calc_pi_hpct_v4_db), sparse_format=sparse_format
+    )
     df = gf.dataframe
 
     for col in gf.dataframe.columns:
